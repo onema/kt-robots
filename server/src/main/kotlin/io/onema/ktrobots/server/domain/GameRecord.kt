77a9a483f04dc8274230b90688c3e36672f2c3da
onema <kinojman@gmail.com>
2021-05-23 20:22:27 -0700

    Update server table name and misc naming updates

--- server/src/main/kotlin/io/onema/ktrobots/server/domain/GameRecord.kt
+++ server/src/main/kotlin/io/onema/ktrobots/server/domain/GameRecord.kt
@@ -17,7 +17,7 @@ import io.onema.ktrobots.commons.domain.Game
 /**
  * This class contains the information that will be saved in the game state table
  */
-@DynamoDBTable(tableName = "KT-Robots-Server-GameTable")
+@DynamoDBTable(tableName = "kt-robots-server-game-table")
 data class GameRecord(
 
     @DynamoDBHashKey(attributeName = "PK")
